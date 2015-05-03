9d163098a60ce00e7a233851168f4fe141ff2e3f
broject <boroo_c@yahoo.com>
2015-05-03 19:10:44 +0900

    first commit

---
+++ AutoHeightLabel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace csharp.controls.autoheightlabel
+{
+    public class AutoHeightLabel : Label
+    {
+        StringFormat _fmt = new StringFormat(StringFormat.GenericTypographic);
+        Rectangle _rc;
+        Size oldClientSize;
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnPaddingChanged(EventArgs e)
+        {
+            base.OnPaddingChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnFontChanged(EventArgs e)
+        {
+            base.OnFontChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnAutoSizeChanged(EventArgs e)
+        {
+            base.OnAutoSizeChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnTextAlignChanged(EventArgs e)
+        {
+            base.OnTextAlignChanged(e);
+            measureHeight();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Pen border = new Pen(ForeColor, 4);
+            e.Graphics.DrawRectangle(border, this.ClientRectangle);
+            border.Dispose();
+
+            if (oldClientSize != ClientSize || _rc == null)
+            {
+                _rc = new Rectangle(new Point(Padding.Left, Padding.Top)
+                , new Size(ClientSize.Width - Padding.Left - Padding.Right
+                    , ClientSize.Height - Padding.Top - Padding.Bottom));
+                oldClientSize = ClientSize;
+            }
+
+            float padx = ((float)this.Size.Width) * (0.05F);
+            float pady = ((float)this.Size.Height) * (0.05F);
+
+            using (var br = new SolidBrush(this.ForeColor))
+            {
+                e.Graphics.DrawString(this.Text, this.Font, br, _rc, _fmt);
+            }
+        }
+
+        private void measureHeight()
+        {
+            switch (TextAlign)
+            {
+                default:
+                case ContentAlignment.TopLeft:
+                case ContentAlignment.MiddleLeft:
+                case ContentAlignment.BottomLeft:
+                    _fmt.Alignment = StringAlignment.Near;
+                    break;
+                case ContentAlignment.TopCenter:
+                case ContentAlignment.MiddleCenter:
+                case ContentAlignment.BottomCenter:
+                    _fmt.Alignment = StringAlignment.Center;
+                    break;
+                case ContentAlignment.TopRight:
+                case ContentAlignment.MiddleRight:
+                case ContentAlignment.BottomRight:
+                    _fmt.Alignment = StringAlignment.Far;
+                    break;
+            }
+
+            using (Graphics g = CreateGraphics())
+            {
+                SizeF size = g.MeasureString(Text, Font, Width - Padding.Left - Padding.Right, _fmt);
+                this.Height = (int)Math.Ceiling(size.Height) + Padding.Top + Padding.Bottom;
+            }
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace csharp.controls.autoheightlabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.autoHeightLabel1 = new csharp.controls.autoheightlabel.AutoHeightLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.autoHeightLabel1);
+            this.panel1.Location = new System.Drawing.Point(64, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 146);
+            this.panel1.TabIndex = 0;
+            // 
+            // autoHeightLabel1
+            // 
+            this.autoHeightLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoHeightLabel1.Location = new System.Drawing.Point(34, 26);
+            this.autoHeightLabel1.Name = "autoHeightLabel1";
+            this.autoHeightLabel1.Size = new System.Drawing.Size(208, 109);
+            this.autoHeightLabel1.TabIndex = 0;
+            this.autoHeightLabel1.Text = "Миний текстийг солиорой...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(401, 482);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private AutoHeightLabel autoHeightLabel1;
+
+    }
+}
+
